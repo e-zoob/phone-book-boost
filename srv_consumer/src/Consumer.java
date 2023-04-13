59bd16d72f7d9426c23d815c3aeccc165a49e3a4
enry <enricozob@gmail.com>
2023-04-13 21:32:26 +0200

    add base consumer

---
+++ srv_consumer/src/Consumer.java
@@ -0,0 +1,22 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Consumer {
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare("phone-book", false, false, false, null);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(message);
+        };
+        channel.basicConsume("phone-book", true, deliverCallback, consumerTag -> {});
+    }
+
+}
