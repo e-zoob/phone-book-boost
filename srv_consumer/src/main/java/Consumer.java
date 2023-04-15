72fddc9c1571155e590acbdd50b1b78f5eb52332
enry <enricozob@gmail.com>
2023-04-15 11:20:48 +0200

    add base maven project

--- srv_consumer/src/main/java/Consumer.java
+++ srv_consumer/src/main/java/Consumer.java
@@ -3,6 +3,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 public class Consumer {
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -13,7 +15,7 @@ public class Consumer {
         channel.queueDeclare("phone-book", false, false, false, null);
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println(message);
         };
         channel.basicConsume("phone-book", true, deliverCallback, consumerTag -> {});
