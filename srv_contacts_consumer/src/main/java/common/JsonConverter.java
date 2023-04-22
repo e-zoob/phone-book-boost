94b7d3204a1049970ac9afb6e78b76320d5bb649
enry <enricozob@gmail.com>
2023-04-22 09:07:58 +0200

    fix: redo consumer

---
+++ srv_contacts_consumer/src/main/java/common/JsonConverter.java
@@ -0,0 +1,13 @@
+package common;
+import com.google.gson.Gson;
+import domain.Contact;
+
+public class JsonConverter {
+    private static Gson gson;
+    //    public JsonConverter(){
+//        this.gson = new Gson();
+//    }
+    public static Contact Deserialize(String message, Gson gson){
+        return gson.fromJson(message, Contact.class);
+    }
+}
