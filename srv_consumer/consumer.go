b8341893b3ea6ae6e9624864031a46a2aa74c60c
Enrico Zoboli <enricozob@gmail.com>
2023-05-01 10:38:43 +0200

    first implementation go consumer

---
+++ srv_consumer/consumer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"phone-book",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Failed to register a consumer")
+
+}
---
+++ srv_consumer/go.mod
@@ -0,0 +1,5 @@
+module phone-book-boost/srv_consumer
+
+go 1.20
+
+require github.com/rabbitmq/amqp091-go v1.8.0
