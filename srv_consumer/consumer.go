917c29b566c1b487065d1017a217d392a5628240
Enrico Zoboli <enricozob@gmail.com>
2023-05-01 11:32:43 +0200

    working simple consumer

--- srv_consumer/consumer.go
+++ srv_consumer/consumer.go
@@ -44,4 +44,13 @@ func main() {
 
 	failOnError(err, "Failed to register a consumer")
 
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
