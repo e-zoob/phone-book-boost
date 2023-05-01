6abae2f261ca95db128ab7b0d6ae1edc31ffb37a
Enrico Zoboli <enricozob@gmail.com>
2023-05-01 18:23:42 +0200

    change host

--- srv_consumer/consumer.go
+++ srv_consumer/consumer.go
@@ -24,7 +24,7 @@ type Contact struct {
 
 func main() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
