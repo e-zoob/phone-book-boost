0c49d195ae0ddfdc4397584e30e1ca7d90d97536
Enrico Zoboli <83153234+e-zoboli@users.noreply.github.com>
2023-05-06 16:49:10 +0200

    Feature/go consumer (#9)
    * remove java consumer
    
    * first implementation go consumer
    
    * working simple consumer
    
    * add contact unmarshalling
    
    * add mongo to docker compose
    
    * add mongo client connection
    
    * add contact insertion on mongo
    
    * feat: add Dockerfile, add Makefile
    
    * change host
    
    * add restart policy to consumer
    
    * move contact struct, update Dockerfile/Makefile
    
    * change host mongo

---
+++ srv_consumer/consumer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"phone-book",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Failed to register a consumer")
+	ctx := context.TODO()
+	opts := options.Client().ApplyURI("mongodb://mongo:27017")
+
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(ctx)
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			var contact Contact
+			if err := json.Unmarshal(d.Body, &contact); err != nil {
+				log.Println("Failed to unmarshal:", err)
+			} else {
+				coll := client.Database("phone-contacts").Collection("contacts")
+				_, err = coll.InsertOne(context.TODO(), contact)
+				if err != nil {
+					panic(err)
+				}
+				log.Printf("Contact saved")
+			}
+		}
+	}()
+
+	log.Printf(" [*] Consumer is running. To exit press CTRL+C")
+	<-forever
+}
