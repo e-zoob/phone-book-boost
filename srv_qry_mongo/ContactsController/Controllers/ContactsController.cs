c3997883da0c918baa815cd7a1a2333b24b7d2eb
Francesco <cicciodb@hotmail.it>
2023-11-01 22:46:49 +0100

    ContactInfo class

--- srv_qry_mongo/ContactsController/Controllers/ContactsController.cs
+++ srv_qry_mongo/ContactsController/Controllers/ContactsController.cs
@@ -55,12 +55,17 @@ namespace ContactsController.Controllers
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
+        [BsonElement("0")]
+        public ContactInfo? Info { get; set; }
+    }
+
+    public class ContactInfo
+    {
         [BsonElement("name")]
         public string? Name { get; set; }
 
         [BsonElement("phone")]
         public string? Phone { get; set; }
-
     }
 
 }
